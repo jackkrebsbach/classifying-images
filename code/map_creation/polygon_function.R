96d09090fdcf13041cbc8869eb7ecb5bcdecc930
Jack Krebsbach <krebsbachjackson@gmail.com>
2022-11-05 12:22:13 -0700

    clean up map creation code

--- code/map_creation/polygon_function.R
+++ code/map_creation/polygon_function.R
@@ -1,29 +1,7 @@
-
-
-# Takes 2-d vector and converts it to 3 dimensions
-make3D <- function(t) {
-  return(c(t[1], t[2], 0))
-}
-
-cross.product <- function(x, y) {
-  x <- make3D(x)
-  y <- make3D(y)
-  
-  boolean <- function(x) {
-    if (x[3] > 0) {
-      return("TRUE")
-    }
-    else{
-      return("FALSE")
-    }
-  }
-  return(boolean(cross(x, y)))
-}
-
-# Quadrant (0-3) is a subset of the quadrat
+# quadrant (0-3) is a region within the quadrat
 # See https://docs.google.com/document/d/1fZsO6068yEvLJnU3if5tpVF9bU1i7cqadtJvj_2KteU/edit
 #
-Polygon.Function <- function(Quadrant, coordinates) {
+Polygon.Function <- function(quadrant, coordinates) {
   c <- slot(subset(coordinates, corner == "c"), "coords")
   c <- c(as.numeric(c[1]), as.numeric(c[2]))
   i <- slot(subset(coordinates, corner == "i"), "coords")
@@ -31,7 +9,7 @@ Polygon.Function <- function(Quadrant, coordinates) {
   ii <- slot(subset(coordinates, corner == "ii"), "coords")
   ii <- c(as.numeric(ii[1]), as.numeric(ii[2]))
   
-  # Incase we mislabeled corners 1 and 2 we flip them.
+  # In case we mislabeled corners 1 and 2 we flip them.
   if (cross.product(ii - c, i - c)) {
     i <- i
     ii <- ii
@@ -41,7 +19,7 @@ Polygon.Function <- function(Quadrant, coordinates) {
     ii <- i
   }
   
-  # Computing all coordinates that we need
+  # Computing all coordinates that we need for the four quadrants
   iii <- i + (ii - c)
   s1 <- (c + i) / 2
   s2 <- (i + iii) / 2
@@ -49,24 +27,42 @@ Polygon.Function <- function(Quadrant, coordinates) {
   s4 <- (c + ii) / 2
   m <-  (c + iii) / 2
   
-  # Compute coordinates for the Quadrant we want
-  if (Quadrant == 0) {
+  # Create a matrix with the coordinates of specified quadrant
+  if (quadrant == 0) {
     coords = matrix(c(c, s1, m, s4, c), ncol = 2, byrow = TRUE)
   }
-  if (Quadrant == 1) {
+  if (quadrant == 1) {
     coords = matrix(c(s1, i, s2, m, s1), ncol = 2, byrow = TRUE)
   }
-  if (Quadrant == 2) {
+  if (quadrant == 2) {
     coords = matrix(c(s4, m, s3, ii, s4), ncol = 2, byrow = TRUE)
   }
-  if (Quadrant == 3) {
+  if (quadrant == 3) {
     coords = matrix(c(m, s2, iii, s3, m), ncol = 2, byrow = TRUE)
   }
-  if (Quadrant == 'total') {
+  if (quadrant == 'total') {
     coords = matrix(c(c, i, iii, ii, c), ncol = 2, byrow = TRUE)
   }
   
   P1 = Polygon(coords)
   quadrant <- SpatialPolygons(list(Polygons(list(P1), ID = "a")))
   return(quadrant)
-}                                                                                                                    
+}
+
+
+# Takes 2-d vector and converts it to 3 dimensions
+to_3d <- function(t) {
+  return(c(t[1], t[2], 0))
+}
+
+
+cross.product <- function(x, y) {
+  z <- cross(to_3d(x), to_3d(y))
+  
+  if (z[3] > 0) {
+    return(TRUE)
+  }
+  else{
+    return(FALSE)
+  }
+}
--- code/misc/numbering-polygons.R
+++ code/misc/numbering-polygons.R
@@ -1,15 +1,19 @@
-
-# Get working directory
+# Check working directory to ensure script can find the json data.
 getwd()
 
+quadrats <-
+  sprintf("clean_data/quadrats/quadrat%02d/rgb.json", seq(34, 83, 1))
 
-quadrats <- sprintf("clean_data/quadrats/quadrat%02d/rgb.json", seq(34, 83, 1))
-
-for( i in 1:length(quadrats)){
+for (i in 1:length(quadrats)) {
   json <- jsonlite::read_json(quadrats[[i]])
-    for( j in 1:length(json$shapes)){
-      json$shapes[[j]]$group_id <- j
-    }
-json %>%
-  jsonlite::write_json(quadrats[[i]], auto_unbox =TRUE, null = "null", pretty =TRUE)
+  
+  for (j in 1:length(json$shapes)) {
+    json$shapes[[j]]$group_id <- j
+  }
+  
+  json %>%
+    jsonlite::write_json(quadrats[[i]],
+                         auto_unbox = TRUE,
+                         null = "null",
+                         pretty = TRUE)
 }
