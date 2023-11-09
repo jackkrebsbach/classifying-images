a6f80da08664d28528101d408cd6ff5fef84f5cd
Jack Krebsbach <krebsbachjackson@gmail.com>
2023-11-09 09:22:08 -0500

    summary statistics for pred map

--- code/extract-features/library/glcm-texture.R
+++ code/extract-features/library/glcm-texture.R
@@ -1,9 +1,10 @@
 library(stringr)
-library(raster)
-library(glcm)
+library(terra)
+library(GLCMTextures)
+library(tictoc)
 source("code/lib/parse-file-path.R")
 
-calculate_texture <- function(inpath, window = 5, statistic = "contrast", layer = 1L, doComp = TRUE) {
+calculate_texture <- function(inpath, window = 5, statistic = "glcm_contrast", layer = 1L, doComp = TRUE) {
   #Texture Path to Write
   out_path <- getTexturePath(inpath, window, statistic, layer)
   
@@ -13,19 +14,31 @@ calculate_texture <- function(inpath, window = 5, statistic = "contrast", layer 
   }
   
   if (doComp) {
+    inpath<- "./clean_data/quadrats/quadrat34/rgb.tif"
+    statistic <- "glcm_contrast"
+    layer <- 1L
+    window <- 5
     #Take in file and calculate texture
-    inpath %>%
-      raster(band = layer) %>%
-      glcm(
-        statistics = statistic,
-        window = c(window, window),
-        shift = list(c(0, 1), c(1, 1), c(1, 0), c(1, -1))
-      ) %>%
-      writeRaster(filename = out_path,
+    tic()
+    red <-  inpath %>%
+      terra::rast(lyrs = layer)
+    
+    contrast <- glcm_textures(
+        red,
+        metrics = statistic,
+        w = c(window, window),
+        n_levels = 16,
+        shift = list(c(0, 1), c(1, 1), c(1, 0), c(1, -1)),
+        quantization = "equal prob"
+      )# %>%
+    
+    toc()
+    
+    
+      terra::writeRaster(filename = out_path,
                   format = "GTiff",
                   overwrite = TRUE)
   }
-  
   return(out_path)
 }
 
--- code/sample-pixels/extract-pixels.R
+++ code/sample-pixels/extract-pixels.R
@@ -1,25 +1,22 @@
-#library(exactextractr)
-#library(raster)
+library(terra)
 
-# extract_pixels <-
-#   function(image_path, data) {
-#     image <- raster::brick(image_path)
-#     extent(image) <- c(0, 4032, 0, 3024) 
-#     pixels <-
-#       exactextractr::exact_extract(image, data, include_cell = TRUE) %>%
-#       bind_rows() %>%
-#       as_tibble()
-#     return(pixels)
-#   }
+extract_pixels <- function(image_path, polygon){
+  image <- terra::rast(image_path)
+  poly_info <- polygon %>% 
+    filter(row_number() == 1)%>%
+    st_drop_geometry()
+  pixels <- terra::extract(image, polygon, cells = TRUE, extent = c(0, 4032, 0, 3024)) %>%
+    cross_join(poly_info) %>%
+    dplyr::select(-ID)
+  return(pixels)
+}
 
-
-extract_pixels <-
-  function(image_path, data) {
-    image <- terra::rast(image_path)
-    extent(image) <- c(0, 4032, 0, 3024) 
-    pixels <-
-      exactextractr::exact_extract(image, data, include_cell = TRUE) %>%
-      bind_rows() %>%
-      as_tibble()
-    return(pixels)
-  }
+# image_path <- "./clean_data/quadrats/quadrat34/rgb.tif"
+# polys <- readRDS("./clean_data/polys_sf.rds")
+# 
+# poly <- polys %>% filter(quadrat == 1)
+# pixels <- extract_pixels(image_path, poly)
+# 
+# print(format(object.size(pixels), units = "MB"))
+# print(nrow(pixels))
+# print(sum(poly$area))
